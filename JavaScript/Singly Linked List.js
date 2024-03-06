5b59c31994d05a14a32579cb8b053f08975f0724
Yeshwanth M R <127304570+Royal-Phoenix@users.noreply.github.com>
2024-03-06 16:08:38 +0530

    Update Singly Linked List.js

--- JavaScript/Singly Linked List.js
+++ JavaScript/Singly Linked List.js
@@ -1,7 +1,7 @@
 class Node {
     constructor(data) {
         this.data = data;
-        this.next = data;
+        this.next = null;
     }
 }
 
@@ -9,9 +9,48 @@ class LinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.nodes = 0;
     }
-    insert(data) {
-        let newNode = new Node(data)
+    insertStart(data) {
+        const newNode = new Node(data);
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.nodes += 1;
+    }
+    insertMid(pos, data) {
+        if (this.head == null) {
+            const newNode = new Node(data);
+            this.head = newNode;
+            this.tail = newNode;
+            this.nodes += 1;
+        }
+        else {
+            if (pos == 1) {
+                this.insertStart(data);
+            }
+            else if (pos >= this.nodes) {
+                this.insertEnd(data);
+            }
+            else {
+                const newNode = new Node(data);
+                let temp = this.head;
+                for (let i=0; i<pos-2; i++) {
+                    temp = temp.next;
+                }
+                newNode.next = temp.next;
+                temp.next = newNode;
+                this.nodes += 1;
+            }
+        }
+    }
+    insertEnd(data) {
+        const newNode = new Node(data);
         if (this.head == null) {
             this.head = newNode;
             this.tail = newNode;
@@ -20,20 +59,63 @@ class LinkedList {
             this.tail.next = newNode;
             this.tail = newNode;
         }
+        this.nodes += 1;
+    }
+    deleteStart() {
+        if (this.head == null) {
+            console.log('Empty Linked List');
+        }
+        else {
+            const temp = this.head;
+            this.head = this.head.next;
+            temp.next = null;
+            this.nodes -= 1;
+        }
+    }
+    deleteMid(pos) {
+        if (this.head == null) {
+            console.log('Empty Linked List');
+        }
+        else {
+            if (pos == 1) {
+                this.deleteStart();
+            }
+            else if (pos >= this.nodes) {
+                this.deleteEnd();
+            }
+            else {
+                let curr = this.head, temp = this.head.next;
+                for (let i=0; i<pos-2; i++) {
+                    curr = curr.next;
+                    temp = temp.next;
+                }
+                curr.next = temp.next;
+                temp.next = null;
+                this.nodes -= 1;
+            }
+        }
+    }
+    deleteEnd() {
+        if (this.head == null) {
+            console.log('Empty Linked List');
+        }
+        else {
+            let temp = this.head;
+            for (let i=0; i<this.nodes-2; i++) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            this.tail = temp;
+            this.nodes -= 1;
+        }
     }
     display() {
         let temp = this.head;
+        let data = ``;
         while (temp !== null) {
-            console.log(temp.data);
+            data += `${temp.data}->`
             temp = temp.next;
         }
+        console.log(data);
     }
-}
-
-var obj = new LinkedList();
-obj.insert(10);
-obj.insert(20);
-obj.insert(30);
-function func() {
-    obj.display()
 }
