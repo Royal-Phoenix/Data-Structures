ecde4ebf0c5fbb275eefcddae943bfc15dca4ccb
Yeshwanth M R <127304570+Royal-Phoenix@users.noreply.github.com>
2024-03-07 15:43:42 +0530

    Create Queue.js

---
+++ JavaScript/Queue.js
@@ -0,0 +1,62 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.front = null;
+        this.rear = null;
+        this.nodes = 0;
+    }
+    enqueueValue(data) {
+        const newNode = new Node(data);
+        if (this.front === null) {
+            this.front = newNode;
+            this.rear = newNode;
+        }
+        else {
+            this.rear.next = newNode;
+            this.rear = newNode;
+        }
+        this.nodes += 1;
+    }
+    dequeueValue() {
+        if (this.front === null)
+            return 'Empty Queue';
+        else {
+            const temp = this.front;
+            this.front = this.front.next;
+            temp.next = null;
+            this.nodes -= 1;
+            return temp;
+        }
+    }
+    peekValue() {
+        return this.front;
+    }
+    searchValue(data) {
+        let temp = this.front, pos = 1;
+        while (temp !== null) {
+            if (temp.data == data) {break;}
+            temp = temp.next;
+            pos += 1;
+        }
+        if (pos > this.nodes) {return null;}
+        else {return pos;}
+    }
+    display() {
+        if (this.bottom === null)
+            console.log('Empty Queue');
+        else {
+            let temp = this.front, data = ``;
+            while (temp !== null) {
+                data += `${temp.data} `;
+                temp = temp.next;
+            }
+            console.log(data);
+        }
+    }
+}
