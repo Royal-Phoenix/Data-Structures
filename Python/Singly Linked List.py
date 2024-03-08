2cd996bdc50e49ebc8ed11a9aa740be9f89aa84d
Yeshwanth M R <127304570+Royal-Phoenix@users.noreply.github.com>
2024-03-08 11:37:51 +0530

    Update Singly Linked List.py

--- Python/Singly Linked List.py
+++ Python/Singly Linked List.py
@@ -1,91 +1,93 @@
 class Node:
     def __init__(self, data):
-       self.data = data
-       self.next = None
+       self.data, self.next = data, None
 
 class SinglyLinkedList:
     def __init__(self):
-        self.head = None
-
-    def Create(self):
-        n = int(input("Enter the Number of Elements : "))
-        for i in range(n):
-            data = int(input("Enter an Element : "))
-            obj.Insert_End(data)
+        self.head, self.tail, self.nodes = None, None, 0
     
-    def Display(self):
+    def insertStart(self, data):
+        new = Node(data)
         if self.head is None:
-            print("The Linked List is Empty")
+            self.head, self.tail = new, new
+        else:
+            new.next = self.head
+            self.head = new
+        self.nodes += 1
+    
+    def insertMid(self, pos, data):
+        if self.head is None:
+            new = Node(data)
+            self.head, self.tail = new, new
+            self.nodes += 1
+        else:
+            if pos == 1:
+                self.insertStart(data)
+            elif pos >= self.nodes:
+                self.insertEnd(data)
+            else:
+                new, temp = Node(data), self.head
+                for _ in range(pos-2):
+                    temp = temp.next
+                new.next = temp.next
+                temp.next = new
+                self.nodes += 1
+    
+    def insertEnd(self, data):
+        new = Node(data)
+        if self.head is None:
+            self.head, self.tail = new, new
+        else:
+            self.tail.next = new
+            self.tail = new
+        self.nodes += 1
+    
+    def deleteStart(self):
+        if self.head is None:
+            return 'Empty Singly Linked List'
         else:
             temp = self.head
-            while temp:
-                print(temp.data,"-->",end = "")
+            self.head = self.head.next
+            temp.next = None
+            self.nodes -= 1
+            return temp
+    
+    def deleteMid(self, pos):
+        if self.head is None:
+            return 'Empty Singly Linked List'
+        else:
+            if pos == 1:
+                self.deleteStart()
+            elif pos >= self.nodes:
+                self.deleteEnd()
+            else:
+                curr, temp = self.head, self.head.next
+                for _ in range(pos-2):
+                    curr, temp = curr.next, temp.next
+                curr.next = temp.next
+                temp.next = None
+                self.nodes -= 1
+                return temp
+    
+    def deleteEnd(self):
+        if self.head is None:
+            return 'Empty Singly Linked List'
+        else:
+            curr = self.head
+            for i in range(self.nodes-2):
+                curr = curr.next
+            temp = curr.next
+            curr.next = None
+            self.tail = temp
+            self.nodes -= 1
+            return temp
+    
+    def display(self):
+        if self.head is None:
+            print('Empty Singly Linked List')
+        else:
+            temp = self.head
+            while temp is not None:
+                print(temp.data, end='-->')
                 temp = temp.next
             print()
-    
-    def Insert_Beg(self,data):
-        new = Node(data)
-        new.next = self.head
-        self.head = new
-    
-    def Insert_Mid(self,pos,data):
-        new = Node(data)
-        temp = self.head
-        for i in range(pos-1):
-            temp = temp.next
-        new.next = temp.next
-        temp.next = new
-    
-    def Insert_End(self,data):
-        new = Node(data)
-        if self.head is None:
-            self.head = new
-        else:
-            temp = self.head
-            while temp.next:
-                temp = temp.next
-            temp.next = new
-    
-    def Delete_Beg(self):
-        temp = self.head
-        self.head = self.head.next
-        temp.next = None
-    
-    def Delete_Mid(self,pos):
-        temp = self.head.next
-        prev = self.head
-        for i in range(1,pos):
-            temp = temp.next
-            prev = prev.next
-        prev.next = temp.next
-    
-    def Delete_End(self):
-        temp = self.head.next
-        prev = self.head
-        while temp.next is not None:
-            temp = temp.next
-            prev = prev.next
-        prev.next = None
-
-obj = SinglyLinkedList()
-obj.Create()
-obj.Display()
-print("Singly Linked List Creation\n")
-obj.Insert_Beg(20)
-obj.Display()
-print("Singly Linked List Insertion at Beginning\n")
-obj.Insert_Mid(3,30)
-obj.Display()
-print("Singly Linked List Inserion at Middle at Position 3\n")
-obj.Insert_End(40)
-obj.Display()
-print("Singly Linked List Insertion at End\n")
-obj.Delete_Beg()
-obj.Display()
-print("Singly Linked List Deletion at Beginning\n")
-obj.Delete_Mid(2)
-obj.Display()
-print("Singly Linked List Deletion at Middle at Position 2\n")
-obj.Delete_End()
-obj.Display()
-print("Singly Linked List Deletion at End")
